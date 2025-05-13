c13cd6ff5d34f71cc3579cb5740e9e3c8b2374d3
osmzoso <glaeser@p200300c80f1f6363108fc8b5583eb1a9.dip0.t-ipconnect.de>
2025-05-13 13:55:00 +0200

    Testscripts added

---
+++ test/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# != 1 ]; then
+    echo "Test pbf2sqlite"
+    echo "Read OpenStreetMap .osm.pbf file and compare the databases"
+    echo "Usage:"
+    echo "$0 OSM_PBF_FILE"
+    exit 1
+fi
+test_dir=$(dirname "$1")
+test_osm_file=$(basename "$1")
+
+echo "Test dir  : " $test_dir
+echo "Test file : " $test_osm_file
+
+./test1_read_data.sh $test_dir $test_osm_file
+./test2_compare_databases.py $test_dir/osm_py.db $test_dir/osm_c.db
