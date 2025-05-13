2d52eaae5d3e18e11711bf8b54aa4a5d95593eb1
osmzoso <glaeser@p200300c80f1f634995e716e6094a776f.dip0.t-ipconnect.de>
2025-05-13 13:28:33 +0200

    help function

--- src/pbf2sqlite.c
+++ src/pbf2sqlite.c
@@ -1,52 +1,29 @@
 /*
-/ pbf2sqlite.c
-/
-/ libreadosm Sample code
-/
-/ Author: Sandro Furieri a.furieri@lqt.it
-/
-/ ------------------------------------------------------------------------------
-/
-/ Version: MPL 1.1/GPL 2.0/LGPL 2.1
-/
-/ The contents of this file are subject to the Mozilla Public License Version
-/ 1.1 (the "License"); you may not use this file except in compliance with
-/ the License. You may obtain a copy of the License at
-/ http://www.mozilla.org/MPL/
-/
-/ Software distributed under the License is distributed on an "AS IS" basis,
-/ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-/ for the specific language governing rights and limitations under the
-/ License.
-/
-/ The Original Code is the ReadOSM library
-/
-/ The Initial Developer of the Original Code is Alessandro Furieri
-/
-/ Portions created by the Initial Developer are Copyright (C) 2012-2017
-/ the Initial Developer. All Rights Reserved.
-/
-/ Contributor(s):
-/
-/ Alternatively, the contents of this file may be used under the terms of
-/ either the GNU General Public License Version 2 or later (the "GPL"), or
-/ the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-/ in which case the provisions of the GPL or the LGPL are applicable instead
-/ of those above. If you wish to allow use of your version of this file only
-/ under the terms of either the GPL or the LGPL, and not to allow others to
-/ use your version of this file under the terms of the MPL, indicate your
-/ decision by deleting the provisions above and replace them with the notice
-/ and other provisions required by the GPL or the LGPL. If you do not delete
-/ the provisions above, a recipient may use your version of this file under
-/ the terms of any one of the MPL, the GPL or the LGPL.
-/
+** pbf2sqlite - Reads OpenStreetMap PBF data into a SQLite database
 */
-
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <sqlite3.h>
 #include <readosm.h>
+
+void show_help() {
+  fprintf(stderr,
+  "pbf2sqlite 0.1\n"
+  "\n"
+  "Reads OpenStreetMap PBF data into a SQLite database.\n"
+  "\n"
+  "Usage:\npbf2sqlite DATABASE [OPTION ...]\n"
+  "\n"
+  "Options:\n"
+  "  read FILE    Reads FILE into the database\n"
+  "               (When FILE is -, read stdin)\n"
+  "  rtree        Add R*Tree indexes\n"
+  "  addr         Add address tables\n"
+  "  graph        Add graph table\n"
+  "\n"
+  );
+  fprintf(stderr, "(SQLite %s is used)\n\n", sqlite3_libversion());
+}
 
 /*
 ** Public variables
@@ -314,9 +291,9 @@ int main (int argc, char *argv[]) {
   const void *osm_handle;
   int ret;
 
-  if (argc != 3) {
-    fprintf (stderr, "Usage:\npbf2sqlite DATABASE OSM_PBF_FILE\n");
-    return -1;
+  if( argc!=3 ) {
+    show_help();
+    return EXIT_FAILURE;
   }
 
   /* STEP #0 Open database */
@@ -366,8 +343,6 @@ int main (int argc, char *argv[]) {
     fprintf (stderr, "PARSE error: %d\n", ret);
     goto stop;
   }
-
-  fprintf (stderr, "Ok, OSM input file successfully parsed\n");
 
 stop:
   /*
