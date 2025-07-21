69f7133626f7bea5d5f0adf1c96ebbd529f4a25a
osmzoso <glaeser@p200300c80f1f6314791b11f7dadf51b6.dip0.t-ipconnect.de>
2025-07-21 12:15:53 +0200

    speedtest revised

--- test/test3_speed.sh
+++ test/test3_speed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Test3: Compare speed
+# Test3: Speedtest
 #
 if [ $# != 2 ]; then
     echo "error: no test directory and osm file specified"
@@ -12,16 +12,28 @@ test_dir=$1
 osm_file=$2
 
 echo "-----------------------------------------------------------------"
-echo "Test3: Compare speed"
+echo "Test3: Speedtest"
 echo "-----------------------------------------------------------------"
 echo "Test dir : $test_dir"
 echo "OSM file : $osm_file"
 
-rm -f $test_dir/osm_speed1.db $test_dir/osm_speed2.db
+#echo "Compare with predecessor osm2sqlite:"
+#echo "read with osmium + osm2sqlite (predecessor of pbf2sqlite)..."
+#rm -f $test_dir/osm_speed.db
+#time -p osmium cat $osm_file -f osm -o - | osm2sqlite osm_speed.db read -
+#echo "read with pbf2sqlite..."
+#rm -f $test_dir/osm_speed.db
+#time -p pbf2sqlite osm_speed.db read $osm_file
 
-echo "read with osmium + osm2sqlite (predecessor of pbf2sqlite)..."
-time -p osmium cat $osm_file -f osm -o - | osm2sqlite osm_speed1.db read -
+rm -f $test_dir/osm_speed.db
 
-echo "read with pbf2sqlite..."
-time -p pbf2sqlite osm_speed2.db read $osm_file
- 
+echo "Time required for option read:"
+time -p pbf2sqlite osm_speed.db read $osm_file
+echo "Time required for option addr:"
+time -p pbf2sqlite osm_speed.db addr
+echo "Time required for option rtree:"
+time -p pbf2sqlite osm_speed.db rtree
+echo "Time required for option graph:"
+time -p pbf2sqlite osm_speed.db graph
+
+rm -f $test_dir/osm_speed.db
