89ac654a85af5cddb98d61ea33caac7e4a369b23
osmzoso <glaeser@p200300c80f1f6348c501b27a70de8bef.dip0.t-ipconnect.de>
2025-07-30 17:42:01 +0200

    test scripts removed

--- fill_graph_permit/compare.sql
+++
@@ -1,9 +0,0 @@
-/*
-**
-*/
-SELECT 'records   : '||count(*) FROM graph;
-SELECT 'equal     : '||count(*) FROM graph WHERE permit=permit_v2;
-SELECT 'not equal : '||count(*) FROM graph WHERE permit!=permit_v2;
-
-.mode table
-SELECT * FROM graph WHERE permit!=permit_v2 LIMIT 10;
