983f5aa73d1f887b67300cd9c84c0175fe72d987
osmzoso <glaeser@p200300c80f1f6316f1313b32421c7b5b.dip0.t-ipconnect.de>
2025-08-08 08:19:37 +0200

    sql command outsourced

--- src/opt_data.c
+++ src/opt_data.c
@@ -7,24 +7,10 @@
 #include <stddef.h>
 
 void add_rtree(sqlite3 *db) {
-  rc = sqlite3_exec(
-    db,
-    /*
-    ** Create R*Tree index 'rtree_way'
-    */
-    " CREATE VIRTUAL TABLE rtree_way USING rtree(way_id, min_lat, max_lat, min_lon, max_lon);"
-    " INSERT INTO rtree_way (way_id, min_lat, max_lat, min_lon, max_lon)"
-    " SELECT way_nodes.way_id,min(nodes.lat),max(nodes.lat),min(nodes.lon),max(nodes.lon)"
-    " FROM way_nodes"
-    " LEFT JOIN nodes ON way_nodes.node_id=nodes.node_id"
-    " GROUP BY way_nodes.way_id;"
-    " CREATE VIRTUAL TABLE rtree_node USING rtree(node_id, min_lat, max_lat, min_lon, max_lon);"
-    " INSERT INTO rtree_node (node_id, min_lat, max_lat, min_lon, max_lon)"
-    " SELECT DISTINCT nodes.node_id,nodes.lat,nodes.lat,nodes.lon,nodes.lon"
-    " FROM nodes"
-    " LEFT JOIN node_tags ON nodes.node_id=node_tags.node_id"
-    " WHERE node_tags.node_id IS NOT NULL;",
-    NULL, NULL, NULL);
+  const char *sql = 
+  #include "add_rtree.sql"
+  ;
+  rc = sqlite3_exec(db, sql, NULL, NULL, NULL);
   if( rc!=SQLITE_OK ) abort_db_error(db, rc);
 }
 
