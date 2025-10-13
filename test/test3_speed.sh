8aee8b31d44d711ad0900b1b110b10f45d13894d
osmzoso <glaeser@p200300c80f343315f1cd090acf6a37de.dip0.t-ipconnect.de>
2025-10-13 11:14:37 +0200

    test3 revised

--- test/test3_speed.sh
+++ test/test3_speed.sh
@@ -6,37 +6,27 @@ if [ $# != 2 ]; then
     echo "Measures the time required for each option."
     echo
     echo "Usage:"
-    echo "$0 TEST_DIR OSM_FILE"
+    echo "$0 PBF_FILE DATABASE_FILE"
     exit 1
 fi
-test_dir=$1
-osm_file=$2
+pbf=$1
+db=$2
 
 echo "-----------------------------------------------------------------"
 echo "Test3: Speedtest"
 echo "-----------------------------------------------------------------"
-echo "Test dir : $test_dir"
-echo "OSM file : $osm_file"
+echo "PBF file      : $pbf"
+echo "DATABASE file : $db"
 
-#echo "Compare with predecessor osm2sqlite:"
-#echo "read with osmium + osm2sqlite (predecessor of pbf2sqlite)..."
-#rm -f $test_dir/osm_speed.db
-#time -p osmium cat $osm_file -f osm -o - | osm2sqlite osm_speed.db read -
-#echo "read with pbf2sqlite..."
-#rm -f $test_dir/osm_speed.db
-#time -p pbf2sqlite osm_speed.db read $osm_file
-
-rm -f $test_dir/osm_speed.db
+rm -f $db
 
 echo "Time required for option read:"
-time -p pbf2sqlite $test_dir/osm_speed.db read $osm_file
+time -p ../build/pbf2sqlite $db read $pbf
 echo "Time required for option index:"
-time -p pbf2sqlite $test_dir/osm_speed.db index
+time -p ../build/pbf2sqlite $db index
 echo "Time required for option rtree:"
-time -p pbf2sqlite $test_dir/osm_speed.db rtree
+time -p ../build/pbf2sqlite $db rtree
 echo "Time required for option addr:"
-time -p pbf2sqlite $test_dir/osm_speed.db addr
+time -p ../build/pbf2sqlite $db addr
 echo "Time required for option graph:"
-time -p pbf2sqlite $test_dir/osm_speed.db graph
-
-rm -f $test_dir/osm_speed.db
+time -p ../build/pbf2sqlite $db graph
