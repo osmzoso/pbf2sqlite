76b2e77807a6f3f8150cd513283d3988c38ce417
osmzoso <glaeser@p200300c80f3433248f9110581fd80587.dip0.t-ipconnect.de>
2025-10-24 11:29:19 +0200

    test4 adapted

--- test/test4_graph.sh
+++ test/test4_graph.sh
@@ -3,11 +3,11 @@ if [ $# != 6 ]; then
     echo
     echo "Test4: Visualizes the table 'graph'"
     echo
-    echo "Generates HTML files with the graph in TEST_DIR."
+    echo "Generates a HTML file with maps of the graph."
     echo "A boundingbox must be given."
     echo
     echo "Usage:"
-    echo "$0 DATABASE LON1 LAT1 LON2 LAT2 TEST_DIR"
+    echo "$0 DATABASE LON1 LAT1 LON2 LAT2 HTML_FILE"
     exit 1
 fi
 db=$1
@@ -15,35 +15,8 @@ lon1=$2
 lat1=$3
 lon2=$4
 lat2=$5
-test_dir=$6
+html_file=$6
 
-permit=0
-filename=$test_dir/graph_all_edges.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
-
-permit=1
-filename=$test_dir/graph_foot.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
-
-permit=2
-filename=$test_dir/graph_bike.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
-
-permit=4
-filename=$test_dir/graph_car.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
-
-permit=8
-filename=$test_dir/graph_paved.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
-
-permit=10
-filename=$test_dir/graph_bike_paved.html
-./html_leaflet_graph.py $db $lon1 $lat1 $lon2  $lat2 $permit course $filename
-firefox $filename
+../build/pbf2sqlite $db vgraph $lon1 $lat1 $lon2 $lat2 $html_file
+firefox $html_file
 
