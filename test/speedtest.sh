891dd9ea48f42a892eaf056afcea2b7650593906
osmzoso <glaeser@p200300c80f366766b59b625c44a805b6.dip0.t-ipconnect.de>
2026-01-02 11:08:43 +0100

    test renamed

--- test/speedtest.sh
+++ test/speedtest.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 if [ $# != 2 ]; then
     echo
-    echo "Test3: Speedtest"
-    echo
+    echo "Speedtest pbf2sqlite"
     echo "Measures the time required for each option."
     echo
     echo "Usage:"
-    echo "$0 DATABASE_FILE PBF_FILE"
+    echo "$0 DATABASE OSM_FILE"
     exit 1
 fi
 db=$1
-pbf=$2
+osm=$2
 
-echo "-----------------------------------------------------------------"
-echo "Test3: Speedtest"
-echo "-----------------------------------------------------------------"
 echo "DATABASE file : $db"
-echo "PBF file      : $pbf"
+echo "OSM file      : $osm"
 
 rm -f $db
 
 echo "Time required for option read:"
-time -p ../build/pbf2sqlite $db read $pbf
+time -p ../build/pbf2sqlite $db read $osm
 echo "Time required for option index:"
 time -p ../build/pbf2sqlite $db index
 echo "Time required for option rtree:"
